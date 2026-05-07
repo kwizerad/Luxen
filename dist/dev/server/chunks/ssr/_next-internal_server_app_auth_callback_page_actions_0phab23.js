0c2a13cee2245fc32ebde56a71c7ec6abcf37525
Luxen User <user@example.com>
2026-05-07 18:02:28 +0300

    Remove dist from git history

--- dist/dev/server/chunks/ssr/_next-internal_server_app_auth_callback_page_actions_0phab23.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/auth/callback/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_auth_callback_page_actions_0phab23.js.map
