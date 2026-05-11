1bbd351847a2fafd9737dfcccccfc69ae8b3e94e
Luxen User <user@example.com>
2026-05-11 18:52:52 +0300

    Remove Google One Tap completely after merge

--- components/client-components.tsx
+++ components/client-components.tsx
@@ -1,55 +1,5 @@
 "use client";
 
-<<<<<<< HEAD
-
-
-import { useEffect, useState } from "react";
-
-import { GoogleOneTap } from "./google-one-tap";
-
-import { PWAInstallPrompt } from "./pwa-install-prompt";
-
-
-
-export function ClientComponents() {
-
-  const [mounted, setMounted] = useState(false);
-
-
-
-  useEffect(() => {
-
-    setMounted(true);
-
-  }, []);
-
-
-
-  // Don't render anything during SSR to avoid hydration mismatch
-
-  if (!mounted) {
-
-    return null;
-
-  }
-
-
-
-  return (
-
-    <>
-
-      <GoogleOneTap />
-
-      <PWAInstallPrompt />
-
-    </>
-
-  );
-
-}
-
-=======
 import { useEffect, useState } from "react";
 import { PWAInstallPrompt } from "./pwa-install-prompt";
 
@@ -71,4 +21,3 @@ export function ClientComponents() {
     </>
   );
 }
->>>>>>> f0bd8b7b5e571701abc6f1ecf61f9c53eb35cfe6
